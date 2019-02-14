724e474eb75e45f365012c7a5ffe978e0d5be3f6
Paulo Solis <a01207593@itesm.mx>
2019-02-14 17:30:22 -0600

    Hola

---
+++ Lab3/app/src/main/java/com/example/paulo/lab1/SecondActivity.java
@@ -0,0 +1,27 @@
+package com.example.paulo.lab1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private TextView tv2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        tv2 = (TextView) findViewById(R.id.tv2);
+        String dato = getIntent().getStringExtra("dato");
+        tv2.setText("Ya quedo"+dato);
+
+
+   }
+
+    public void Anterior(View view){
+        Intent anterior = new Intent(this, MainActivity.class);
+        startActivity(anterior);
+    }
+}
